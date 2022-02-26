0eee1c1cc28a620beb82b36b2a218a0ceb3bafb0
Baidauletov <baidauletov.nr@gmail.com>
2022-02-26 17:34:17 +0600

    Notification task solution

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,65 @@
 export default class NotificationMessage {
+  static stateNotification = null;
 
+  constructor(message = "", { duration = 0, type = "success" } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  getNotification() {
+    return `
+      <div class="notification
+        ${this.type === "success" ? "success" : "error"}
+      ">
+        <div class="inner-wrapper">
+          <div class="notification-header">
+          </div>
+          <div class="notification-body">
+            ${this.message}
+            ${this.setTimer()}
+          </div>  
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement("div");
+
+    element.innerHTML = this.getNotification();
+
+    this.element = element.firstElementChild;
+  }
+
+  show(node = document.body) {
+    if (NotificationMessage.stateNotification) {
+      NotificationMessage.stateNotification.remove();
+    }
+
+    this.timer = setTimeout(() => this.destroy(), this.duration);
+
+    node.append(this.element);
+
+    NotificationMessage.stateNotification = this;
+  }
+
+  setTimer() {
+    return `
+      <div class="timer" style="--value: ${this.duration / 1000}s">
+      </div>
+    `;
+  }
+
+  remove() {
+    this.element.remove();
+    clearTimeout(this.timer);
+  }
+
+  destroy() {
+    this.remove();
+    NotificationMessage.stateNotification = null;
+  }
 }
